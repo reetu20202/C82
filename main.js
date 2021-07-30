bc43278f0e8a15edae1cd9112a664d66037075eb
reetu20202 <67657092+reetu20202@users.noreply.github.com>
2021-07-30 17:54:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-canvas=document.getElementById("myCanvas");
-ctx = canvas.getContext("2d");
-var color="";
-var width="";
-var radius="";
+var canvas=document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var color="blue";
+var width=20;
+var radius=10;
 var current_position_of_mouse_x = 0;
 var current_position_of_mouse_y = 0;
 var mouseEvent = "";
 
-window.addEventListener("mouseDown",myMousedown)
-function myMousedown(e)
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e)
 {
     color  = document.getElementById("color").value;
     width = document.getElementById("width").value;
     radius = document.getElementById("radius").value;
-    mouseEvent = "mouseDown";
+    mouseEvent = "mousedown";
 }
-window.addEventListener("mouseUp",myMouseup)
-function myMouseup(e)
+canvas.addEventListener("mouseup",my_mouseup);
+function my_mouseup(e)
 {
     color  = document.getElementById("color").value;
     width = document.getElementById("width").value;
     radius = document.getElementById("radius").value;
-    mouseEvent = "mouseUp";
+    mouseEvent = "mouseup";
 }
 
-window.addEventListener("mouseLeave",myMouseleave)
-function myMouseleave(e)
+canvas.addEventListener("mouseleave",my_mouseleave);
+function my_mouseleave(e)
 {
     color  = document.getElementById("color").value;
     width = document.getElementById("width").value;
     radius = document.getElementById("radius").value;
-    mouseEvent = "mouseLeave";
+    mouseEvent = "mouseleave";
 }
 
 
 
 
-window.addEventListener("mousemove", myMousemove)
-function myMousemove(e){
+canvas.addEventListener("mousemove", my_mousemove);
+function my_mousemove(e){
     current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
     current_position_of_mouse_y = e.clientY - canvas.offsetTop;
 
-    if (mouseEvent == "myMousedown") {
+    if (mouseEvent == "mousedown") {
         console.log("Current position of x and y coordinates = ");
         console.log("x = " + current_position_of_mouse_x + "y = " +current_position_of_mouse_y);
         ctx.beginPath();
