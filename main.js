b8ad8b9048086f11fcaee425a56a80d131e1cbee
reetu20202 <67657092+reetu20202@users.noreply.github.com>
2021-07-31 18:49:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ var current_position_of_mouse_x = 0;
 var current_position_of_mouse_y = 0;
 var mouseEvent = "";
 
-canvas.addEventListener("mousedown",my_mousedown);
+function submit () {
+        document.getElementById("heade1").innerHTML="Test sucessful"
+}
+
+window.addEventListener("mousedown",my_mousedown);
 function my_mousedown(e)
 {
     color  = document.getElementById("color").value;
@@ -15,7 +19,7 @@ function my_mousedown(e)
     radius = document.getElementById("radius").value;
     mouseEvent = "mousedown";
 }
-canvas.addEventListener("mouseup",my_mouseup);
+window.addEventListener("mouseup",my_mouseup);
 function my_mouseup(e)
 {
     color  = document.getElementById("color").value;
@@ -24,7 +28,7 @@ function my_mouseup(e)
     mouseEvent = "mouseup";
 }
 
-canvas.addEventListener("mouseleave",my_mouseleave);
+window.addEventListener("mouseleave",my_mouseleave);
 function my_mouseleave(e)
 {
     color  = document.getElementById("color").value;
@@ -36,18 +40,19 @@ function my_mouseleave(e)
 
 
 
-canvas.addEventListener("mousemove", my_mousemove);
+window.addEventListener("mousemove", my_mousemove);
 function my_mousemove(e){
     current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
     current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-
+    radius=document.getElementById("radius").value;
     if (mouseEvent == "mousedown") {
         console.log("Current position of x and y coordinates = ");
         console.log("x = " + current_position_of_mouse_x + "y = " +current_position_of_mouse_y);
+        
         ctx.beginPath();
         ctx.strokeStyle = color;
         ctx.lineWidth = width;
-        ctx.arc(current_position_of_mouse_x,current_position_of_mouse_y,radius, 0 , 2 * Math.PI);
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0 , 2 * Math.PI);
         ctx.stroke();
 
     }
